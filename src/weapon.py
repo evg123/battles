6b776abc7f42b53b7980880cac2dc3b0c6c55fd4
Eric VandeGriek <evandegriek@gmail.com>
2019-04-04 19:38:02 -0400

    bow firing mechanics, start of formation rearrange

--- src/weapon.py
+++ src/weapon.py
@@ -2,7 +2,7 @@
 Classes representing weapons like a sword or bow
 """
 from pygame import Vector2
-
+from src.graphics import Colors
 
 class Weapon:
     """Abstract base class"""
@@ -10,11 +10,10 @@ class Weapon:
     def __init__(self):
         # Position of the weapon wielder
         self.pos = Vector2()
-
         # Angle relative to wielder
         self.angle = 0
-
         self.damage = 0
+        self.stationary_time = 0
 
     def update(self, delta):
         pass
@@ -46,7 +45,7 @@ class Sword(Weapon):
     START_DIST_OFFSET = 4
     FINAL_DIST_OFFSET = 8
     START_ANGLE_OFFSET = 0
-    COLOR = (128, 128, 128)
+    COLOR = Colors.darkgrey
 
     def __init__(self):
         super(Sword, self).__init__()
@@ -57,6 +56,7 @@ class Sword(Weapon):
         self.length = 22
         self.width = 5
         self.attack_range = self.length + self.FINAL_DIST_OFFSET
+        self.stationary_time = 0.1
 
         # Offset from wielder
         self.dist_offset = self.START_DIST_OFFSET
@@ -109,29 +109,45 @@ class Sword(Weapon):
 
 
 class Bow(Weapon):
-
-    START_ANGLE_OFFSET = 0
+    COLOR = Colors.brown
+    FIRING_SPEED = 1.2
 
     def __init__(self):
         super(Bow, self).__init__()
         self.arrows = []
-        self.arrow_speed = 600
         self.damage = 20
         self.attack_range = 400
+        self.fire_timer = 0
+        self.stationary_time = 0.5
 
         # Offset from wielder
         self.dist_offset = 10
-        self.angle_offset = self.START_ANGLE_OFFSET
+        self.angle_offset = 0
 
     def update(self, delta):
-        pass
+        self.fire_timer -= delta
+
+        for arrow in self.arrows:
+            arrow.update(delta)
+
+        # Remove spent arrows
+        self.arrows = [arrow for arrow in self.arrows if not arrow.needs_removal()]
 
     def draw(self, renderer):
-        pass
+        norm = Vector2(0, -1)
+        norm.rotate_ip(self.angle)
+        pos = self.pos + norm * self.dist_offset
+        renderer.draw_circle(self.COLOR, pos, 3) #TODO draw arc instead
+
+        for arrow in self.arrows:
+            arrow.draw(renderer)
 
     def activate(self):
-        # Fire an arrow
-        pass
+        if self.fire_timer <= 0:
+            # Fire an arrow
+            arrow = Arrow(self.pos, self.angle)
+            self.arrows.append(arrow)
+            self.fire_timer = self.FIRING_SPEED
 
     def deactivate(self):
         # Can't stop a fired arrow
@@ -139,4 +155,48 @@ class Bow(Weapon):
 
     def hits_circle(self, other_pos, other_radius):
         # Check if any arrow hits the circle
+        for arrow in self.arrows:
+            if arrow.hits_circle(other_pos, other_radius):
+                return True
         return False
+
+class Arrow(Weapon):
+
+    COLOR = Colors.brown
+
+    def __init__(self, pos, angle):
+        super(Arrow, self).__init__()
+        self.pos.x = pos.x
+        self.pos.y = pos.y
+        self.angle = angle
+        self.length = 8
+        self.width = 1
+        self.flight_speed = 400
+        self.max_distance = 400
+        self.distance = 0
+        self.hit = False # has the arrow connected?
+
+    def needs_removal(self):
+        return self.distance > self.max_distance or self.hit
+
+    def update(self, delta):
+        norm = Vector2(0, -1)
+        norm.rotate_ip(self.angle)
+        dist = self.flight_speed * delta
+        new_pos = self.pos + norm * dist
+        self.pos.x = new_pos.x
+        self.pos.y = new_pos.y
+        # Keep track of how far the arrow has gone
+        self.distance += dist
+
+    def draw(self, renderer):
+        norm = Vector2(0, -1)
+        norm.rotate_ip(self.angle)
+        end_pos = self.pos + norm * self.length
+        renderer.draw_line(self.COLOR, self.pos, end_pos, self.width)
+
+    def hits_circle(self, other_pos, other_radius):
+        dist = other_pos.distance_to(self.pos)
+        hit = dist <= other_radius
+        self.hit = True
+        return hit
